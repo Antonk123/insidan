1e2f0d6b11c744be8d5c825f4c13813aefab330a
Anton Kaarle <anton@Macbook-Pro.pitea.local>
2026-01-29 14:36:21 +0100

    Set default safety start date

--- supabase/migrations/20260129174500_add_safety_start_date_setting.sql
+++ supabase/migrations/20260129174500_add_safety_start_date_setting.sql
@@ -1,4 +1,4 @@
 -- Add optional safety counter start date setting
 INSERT INTO public.site_settings (key, value)
-VALUES ('safety_start_date', '')
+VALUES ('safety_start_date', '2025-11-18T00:00:00')
 ON CONFLICT (key) DO NOTHING;
